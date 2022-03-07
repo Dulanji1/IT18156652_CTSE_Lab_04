7508946f9d56b3bba4fa44526e561d869ac5ad00
Dulanji1 <mddulanjicooray@gmail.com>
2022-03-08 03:16:47 +0530

    commit

---
+++ ctse_lab_04_flutter/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:ctse_lab_04_flutter/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Agify' ,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: const Home(),
+      ),
+    );
+  }
+}
